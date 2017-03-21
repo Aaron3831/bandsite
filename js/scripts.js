8ea059fd46f9bc5d4d90291b1fb46dd9164e5a84
Aaron Nguyen and Max Reichman <aaron+max@>
2017-03-21 10:23:07 -0700

    Add JavaScript to hide or show showtimes

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // $("h1").fadeIn(2000);
+  // $(".clickable").click(function() {
+  //   $("#walrus-showing").toggle();
+  //   $("#walrus-hidden").toggle();
+  // });
+  // $(".fadeout").hover(function() {
+  //   $( this ).fadeOut(3000);
+  // });
+  // $("button").click(function() {
+  //   $(".intro").fadeToggle("slow", "linear");
+  // });
+  $(".clickable").click(function() {
+    $(".hidden").toggle();
+  });
+});
